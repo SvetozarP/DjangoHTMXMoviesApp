092c9e56cd03c77d9f4b63be505de976376804f4
SvetozarP <145108913+SvetozarP@users.noreply.github.com>
2025-06-01 13:54:11 +0100

    Added HTMX check green/red if username exists with dynamic checking on registration

--- films/urls.py
+++ films/urls.py
@@ -5,6 +5,12 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('index/', views.IndexView.as_view(), name='index'),
     path('login/', views.Login.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('logout/', views.logout_view, name='logout'),
     path("register/", views.RegisterView.as_view(), name="register")
 ]
+
+htmx_urlpatterns = [
+    path('check_username/', views.check_username, name='index'),
+]
+
+urlpatterns += htmx_urlpatterns
--- films/views.py
+++ films/views.py
@@ -1,9 +1,9 @@
 from django.http.response import HttpResponse, HttpResponsePermanentRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
-from django.views.generic import FormView, TemplateView
-from django.contrib.auth import get_user_model
+from django.views.generic import FormView, TemplateView, RedirectView
+from django.contrib.auth import get_user_model, logout
 
 from films.forms import RegisterForm
 
@@ -22,3 +22,19 @@ class RegisterView(FormView):
     def form_valid(self, form):
         form.save()  # save the user
         return super().form_valid(form)
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("index")
+
+def check_username(request):
+    username = request.POST.get('username')
+    if request.htmx:
+        if get_user_model().objects.filter(username=username).exists():
+            return HttpResponse('<div style="color: red" id="username-error">This username is already exists.</div>')
+
+        else:
+            return HttpResponse('<div style="color: green" id="username-error">This username is available.</div')
+    else:
+        return redirect("login")
