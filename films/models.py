d72fee8659667babe462b422b646bdef98a4efbc
SvetozarP <145108913+SvetozarP@users.noreply.github.com>
2025-06-08 21:49:23 +0100

    Added functionality to drag and drop - reordering films in user's list.

--- films/models.py
+++ films/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.functions import Lower
 
 class User(AbstractUser):
     pass
@@ -8,4 +9,7 @@ class User(AbstractUser):
 class Film(models.Model):
     name = models.CharField(max_length=128, unique=True)
     users = models.ManyToManyField(User, related_name='films')
+
+    class Meta:
+        ordering = [Lower('name')]
 
